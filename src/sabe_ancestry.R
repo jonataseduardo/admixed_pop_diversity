1f3cf60af048a38d3780e042d01213bdf94905b0
jonataseduardo <jonataseduardo@gmail.com>
2019-08-06 16:13:54 -0300

    selecting samples from sabe dataset

---
+++ src/sabe_ancestry.R
@@ -0,0 +1,19 @@
+library(data.table)
+
+sabe_ancestry <- fread('/scratch/genevol/sabe/SABE_Global_ancestry.txt')
+
+s_sabe <- sabe_ancestry[NAM < 0.05 & AFR > 0.1 & AFR < 0.35]
+
+sabe_ancestry[,.(mean(NAM), mean(AFR), mean(EUR))]
+
+s_sabe[,.(.N, mean(NAM), mean(AFR), mean(EUR))]
+
+file_name <- 
+  s_sabe[,  paste0('sample_list_sabe_N_', .N, 
+            '_NAM_', round(mean(NAM), digits = 2), 
+            '_AFR_' , round(mean(AFR), digits = 2),
+            '_EUR_' , round(mean(EUR), digits = 2),
+            '.txt')]
+
+file_path <- '../data/' 
+fwrite(s_sabe[,.(IND)] , paste0(file_path, file_name), col.name = FALSE) 
